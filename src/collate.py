5bc7c325b591b6c78c810e87f85f777a577b79a5
Max Pershin <mepershin@gmail.com>
2022-05-01 10:47:15 +0000

    collate fn

---
+++ src/collate.py
@@ -0,0 +1,27 @@
+import torch
+from dataclasses import dataclass
+
+
+@dataclass
+class Batch:
+    images: torch.Tensor
+    masks: torch.Tensor
+
+    def to(self, device: torch.device) -> 'Batch':
+        return Batch(
+                images=self.images.to(device),
+                masks=self.masks.to(device),
+            )
+
+
+def collate_fn(inputs: list) -> Batch:
+    images = [x['image'] for x in inputs]
+    images = torch.stack(images, dim=0)
+
+    masks = [x['mask'] for x in inputs]
+    masks = torch.stack(masks, dim=0)
+
+    return Batch(
+            images=images,
+            masks=masks,
+        )
